14f6466ffcd80fc0403920d588f2176036a45386
btouellette <btouellette@7b961811-d895-1bb3-e01f-8dd663717c48>
2010-10-08 14:23:15 +0000

    Fixing div by 2 error

--- src/EIGpartition.m
+++ src/EIGpartition.m
@@ -22,12 +22,12 @@ Q = D - A;
 [sorted, Z] = sort(vecs(:,2));
 
 % Compute cut size and ratiocut for relevant partitions
-cutsize = sum(sum(A(Z((S(1)+1):n),Z(1:S(1)))))/2;
+cutsize = sum(sum(A(Z((S(1)+1):n),Z(1:S(1)))));
 ratiocut = cutsize/(S(1)*(n-S(1)));
 cutsize_at = S(1);
 ratiocut_at = S(1);
 for i = (S(1)+1):S(2)
-    new_cutsize = sum(sum(A(Z((i+1):n),Z(1:i))))/2;
+    new_cutsize = sum(sum(A(Z((i+1):n),Z(1:i))));
     if (new_cutsize < cutsize)
         cutsize = new_cutsize;
         cutsize_at = i;
