4465a622085127d6ffc76846aae71b7683593379
btouellette <btouellette@7b961811-d895-1bb3-e01f-8dd663717c48>
2010-11-23 22:34:06 +0000

    Annealing now problem specific

--- trunk/inc/slicing.h
+++ trunk/inc/slicing.h
@@ -1,4 +1,19 @@
 #ifndef PART_SLICING_H_
 #define PART_SLICING_H_
 
+typedef unsigned slicing_string;
+typedef slicing_string slicing_index;
+
+typedef struct {
+	slicing_index ind;
+	char move_type;
+} move;
+
+#define INITIAL_TEMPERATURE 1
+#define COOLING_STEPS 500
+#define COOLING_FRACTION 0.97
+#define STEPS_PER_TEMP 1000
+#define E 2.718
+#define K 0.01
+
 #endif // PART_SLICING_H_
