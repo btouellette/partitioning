74fe381b748c4fe0591058eee338545259ddc633
btouellette <btouellette@7b961811-d895-1bb3-e01f-8dd663717c48>
2010-12-07 17:01:34 +0000

    on to debugging!

--- inc/floor.h
+++ inc/floor.h
@@ -9,8 +9,14 @@ typedef slicing_string slicing_index;
 #define V 255
 #define H 254
 
+// Relative probabilities (integers)
+#define M1_PROB 25
+#define M2_PROB 25
+#define M3_PROB 25
+#define M4_PROB 25
+
 typedef struct {
-	slicing_index ind;
+	slicing_index index;
 	char move_type;
 } move;
 
@@ -19,10 +25,19 @@ typedef struct {
 } block;
 
 #define INITIAL_TEMPERATURE 1
-#define COOLING_STEPS 500
-#define COOLING_FRACTION 0.97
-#define STEPS_PER_TEMP 1000
+#define COOLING_STEPS 500 /* higher improves quality, lower speeds up */
+#define COOLING_FRACTION 0.97 /* same */
+#define STEPS_PER_TEMP 1000 /* same */
 #define E 2.718
 #define K 0.01
+
+#define swap(x,y, type) \
+	do { \
+		type tmp = (x); \
+		(x) = (y); \
+		(y) = tmp; \
+		} while(0)
+#define TRUE  1
+#define FALSE 0
 
 #endif // PART_SLICING_H_
